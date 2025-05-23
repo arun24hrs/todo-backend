b0ddedb0cd494c55762501e0c35d48a0086bc9a9
arun24hrs <2017arun.rana@gmail.com>
2025-05-23 16:11:02 +0530

    updated the task schema

--- todo-backend/Model/tasksModel.js
+++ todo-backend/Model/tasksModel.js
@@ -4,9 +4,9 @@ const taskSchema = mongoose.Schema({
     userID: {type: String, required: true},
     taskName: {type: String, required: true},
     dueDate: {type: String, required: true},
-    isPrior: {type: Boolean, required: true},
-    isComplete: {type: Boolean, required: true},
-    isPending: {type: Boolean, required: true}
+    isPrior: {type: Boolean, required: true, default: false},
+    isComplete: {type: Boolean, required: true, default: false},
+    isPending: {type: Boolean, required: true, default: false}
 });
 
 const TaskModel = mongoose.model("task", taskSchema);
